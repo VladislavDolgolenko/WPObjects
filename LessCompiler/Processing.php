a289e04dc608848ad9758f894106f024c0a9627b
Vladislav Dolgolenko <thesylerman@gmail.com>
2017-11-16 19:20:43 +0200

    +

--- LessCompiler/Processing.php
+++ LessCompiler/Processing.php
@@ -2,7 +2,7 @@
 
 /**
  * @encoding     UTF-8
- * @copyright    Copyright (C) 2016 Torbara (http://torbara.com). All rights reserved.
+ * @copyright    Copyright (C) 2017 Torbara (http://torbara.com). All rights reserved.
  * @license      Envato Standard License http://themeforest.net/licenses/standard?ref=torbara
  * @author       Vladislav Dolgolenko (vladislavdolgolenko.com)
  * @support      support@torbara.com
