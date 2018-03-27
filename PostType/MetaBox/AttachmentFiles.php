4f3d87b9a270c11ab0007fb038fae99959c041d0
Vladislav Dolgolenko <thesylerman@gmail.com>
2018-03-27 14:27:11 +0300

    +

---
+++ PostType/MetaBox/AttachmentFiles.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @encoding     UTF-8
+ * @package      WPObjects
+ * @link         https://github.com/VladislavDolgolenko/WPObjects
+ * @copyright    Copyright (C) 2018 Vladislav Dolgolenko
+ * @license      MIT License
+ * @author       Vladislav Dolgolenko <vladislavdolgolenko.com>
+ * @support      <help@vladislavdolgolenko.com>
+ */
+
+namespace WPObjects\PostType\MetaBox;
+
+class AttachmentFiles extends AbstractMetaBox
+{
+    public function __construct()
+    {
+        $this->setId('attachment-files');
+        $this->setTitle('Attachment files');
+        $this->setPosition('normal');
+        $this->setPriority('default');
+        
+        $template_dir = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'templates';
+        $this->setTemplatePath($template_dir . DIRECTORY_SEPARATOR . 'attachment-files.php');
+    }
+    
+    public function enqueues()
+    {
+        parent::enqueues();
+        $this->getAssetsManager()->enqueueScript('metabox_attributes');
+    }
+}
---
+++ PostType/MetaBox/templates/attachment-files.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @encoding     UTF-8
+ * @package      WPObjects
+ * @link         https://github.com/VladislavDolgolenko/WPObjects
+ * @copyright    Copyright (C) 2018 Vladislav Dolgolenko
+ * @license      MIT License
+ * @author       Vladislav Dolgolenko <vladislavdolgolenko.com>
+ * @support      <help@vladislavdolgolenko.com>
+ */
+
+/* @var $this \WPObjects\PostType\MetaBox\AttachmentFiles */
+
+?>
+
+<div class="msp-bootstrap-wrapper msp-form msp-attch-files">
+    
+    <div class="files">
+        <div class="file">
+            <input type="hidden" name="">
+            <div class="col-sm-1">
+                <i class="fa fa-arrows"></i>
+            </div>
+            <div class="col-sm-6">
+                Name of file
+            </div>
+            <div class="col-sm-4">
+                40mb | .doc
+            </div>
+            <div class="col-sm-1">
+                <i class="fa fa-times"></i>
+            </div>
+            
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    
+    <button class="button button-default button-width">
+        Attach files
+    </button>
+    
+</div>
